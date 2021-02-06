bccdb4fafcca7aaae89299c01e500ca28e703fa5
Wone <w1y45hjcq3@criptext.com>
2021-02-06 19:37:13 +0300

    Add babel config and typescript

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+module.export = {
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: true }]
+  ]
+}
